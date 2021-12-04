f522c6e0117a03c2b21041fd68deb409f5f1f5f7
Emily Dokken <dokkenemily@gmail.com>
2021-12-04 13:36:21 -0800

    add list groups for each time block in a day

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,4 @@
-/*WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
+/*
 WHEN I scroll down
 THEN I am presented with time blocks for standard business hours
 WHEN I view the time blocks for that day
@@ -9,9 +8,12 @@ THEN I can enter an event
 WHEN I click the save button for that time block
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
-THEN the saved events persist*/
+THEN the saved events persist
+*/
 
 var currentDateEl = $("#currentDay");
 var currentDate = moment().format('dddd, MMMM Do YYYY');
 
+// add current date to the page
 currentDateEl.text(currentDate);
+
