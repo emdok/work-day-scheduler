1f7a016b4c595c3d9c96cca1ff60d50dfc4eb8bf
Emily Dokken <dokkenemily@gmail.com>
2021-12-05 01:24:49 -0800

    set each time slot to moment js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,23 @@ var currentDate = moment().format('dddd, MMMM Do YYYY');
 // add current date to the page
 currentDateEl.text(currentDate);
 
+var nineEl = $('#hour-9');
+var tenEl = $('#hour-10');
+var elevenEl = $('#hour-11');
+var twelveEl = $('#hour-12');
+var oneEl = $('#hour-1');
+var twoEl = $('#hour-2');
+var threeEl = $('#hour-3');
+var fourEl = $('#hour-4');
+var fiveEl = $('#hour-4');
+
+nineEl.text(moment().set('hour', 9).format('h A'));
+tenEl.text(moment().set('hour', 10).format('h A'));
+elevenEl.text(moment().set('hour', 11).format('h A'));
+twelveEl.text(moment().set('hour', 12).format('h A'));
+oneEl.text(moment().set('hour', 13).format('h A'));
+twoEl.text(moment().set('hour', 14).format('h A'));
+threeEl.text(moment().set('hour', 15).format('h A'));
+fourEl.text(moment().set('hour', 16).format('h A'));
+fiveEl.text(moment().set('hour', 17).format('h A'));
+
