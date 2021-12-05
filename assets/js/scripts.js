2c7e178f3420eee555ee25001472c54bb60c150e
Emily Dokken <dokkenemily@gmail.com>
2021-12-05 13:11:51 -0800

    create an array of the time blocks and get the data attritube for each to store in an object

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,4 @@
 /*
-WHEN I scroll down
-THEN I am presented with time blocks for standard business hours
 WHEN I view the time blocks for that day
 THEN each time block is color-coded to indicate whether it is in the past, present, or future
 WHEN I click into a time block
@@ -13,27 +11,26 @@ THEN the saved events persist
 
 var currentDateEl = $("#currentDay");
 var currentDate = moment().format('dddd, MMMM Do YYYY');
+var currentTime = moment().format('hh:mm a');
+var tasksArray = [];
 
 // add current date to the page
 currentDateEl.text(currentDate);
 
-var nineEl = $('#hour-9');
-var tenEl = $('#hour-10');
-var elevenEl = $('#hour-11');
-var twelveEl = $('#hour-12');
-var oneEl = $('#hour-1');
-var twoEl = $('#hour-2');
-var threeEl = $('#hour-3');
-var fourEl = $('#hour-4');
-var fiveEl = $('#hour-4');
+//grab all of the timeblocks data attributes
+tasksArray = $.map($('.hours'), function(el) {
+    return {hour: 'timeBlock', value: parseInt($(el).attr('data-hour'))}
+});
 
-nineEl.text(moment().set('hour', 9).format('h A'));
-tenEl.text(moment().set('hour', 10).format('h A'));
-elevenEl.text(moment().set('hour', 11).format('h A'));
-twelveEl.text(moment().set('hour', 12).format('h A'));
-oneEl.text(moment().set('hour', 13).format('h A'));
-twoEl.text(moment().set('hour', 14).format('h A'));
-threeEl.text(moment().set('hour', 15).format('h A'));
-fourEl.text(moment().set('hour', 16).format('h A'));
-fiveEl.text(moment().set('hour', 17).format('h A'));
+console.log(tasksArray);
+
+// write function to check if the time is in the past present or future
+
+
+
+// if time is in the past set a color
+// if time is current set a color
+// if time is in the future set a color
+
+// write function to listen for a click on specific element to save data in text area to local storage
 
